2aa1654dc007eafb9c6a13747bbd932bf18cff15
Jason <jdmarshall@users.noreply.github.com>
2025-12-03 22:06:09 -0800

    Arrange existing tests in the correct file names.

--- test/index.js
+++ test/index.js
@@ -1,95 +1,13 @@
 import { describe, it } from "node:test";
 import * as chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import Util from "../lib/util.js";
+import chaiString from 'chai-string';
+import main from "../lib/index.js";
 
 chai.use(chaiAsPromised);
+chai.use(chaiString);
 
 const expect = chai.expect;
 
-describe("Util", () => {
-  describe("packageName()", () => {
-    it("exists", () => {
-      expect(Util.packageName).to.exist;
-    });
-
-    it("handles packages with slashes", () => {
-      expect(Util.packageName("@babel/core@latest")).to.equal("@babel/core");
-    });
-
-    it("handles packages without organizations", () => {
-      expect(Util.packageName("core@latest")).to.equal("core");
-    });
-
-    it("handles packages without version number organizations", () => {
-      expect(Util.packageName("@babel/core")).to.equal("@babel/core");
-    });
-  });
-
-  describe("headingName()", () => {
-    it("exists", () => {
-      expect(Util.headingName).to.exist;
-    });
-
-    it("grabs everything to the last arrow", () => {
-      let testName = "example ⇒ suite name ⇒ nested suite name ⇒ scenario name"
-
-      expect(Util.headingName("a ⇒ b")).to.equal("a");
-      expect(Util.headingName(testName)).to.equal("example ⇒ suite name ⇒ nested suite name");
-    });
-  });
-
-  describe('asyncExec()', () => {
-    it("exists", () => {
-      expect(Util.asyncExec).to.exist;
-    });
-
-    it("resolves on commands", async () => {
-      await expect(Util.asyncExec('ls -la')).to.eventually.include("package-lock.json");
-    });
-
-    it("rejects on errors", async () => {
-      await expect(Util.asyncExec('ls missing_file')).to.be.rejectedWith("Command failed");
-    });
-  });
-
-  describe('findSlow()', () => {
-    it("exists", () => {
-      expect(Util.findSlow).to.exist;
-    });
-
-    it("handles empty set", () => {
-      expect(() => Util.findSlow()).not.to.throw();
-    });
-
-    it("ignores small discrepancies", () => {
-      let actual = Util.findSlow([
-        [{ opsSec: 22, fastest: true }, { opsSec: 20 }, { opsSec: 21, slowest: true }]
-      ]);
-
-      expect(actual).to.be.empty;
-    });
-
-    it("finds regressions", () => {
-      let actual = Util.findSlow([
-        [{ opsSec: 20, slowest: true }, { opsSec: 20 }, { opsSec: 21, fastest: true }],
-        [{ opsSec: 21, fastest: true }, { opsSec: 20 }, { opsSec: 18.5, slowest: true }]
-      ]);
-
-      expect(actual).have.lengthOf(1);
-      expect(actual[0][2]).to.deep.equal({ opsSec: 18.5, slowest: true });
-    });
-  });
-
-  describe("install()", () => {
-    it("exists", () => {
-      expect(Util.install).to.exist;
-    });
-  });
-
-  describe("chartResults()", () => {
-    it("exists", () => {
-      expect(Util.chartResults).to.exist;
-    });
-  });
+describe("index", () => {
 });
--- test/util.js
+++ test/util.js
@@ -101,3 +101,96 @@ describe("Util integration", () => {
     });
   });
 });
+
+describe("Util integration", () => {
+  describe("chartReport()", () => {
+    it("handles empty results", () => {
+      expect(() => Util.chartResults([])).not.to.throw();
+    });
+
+    it("handles results", () => {
+      let result = Util.chartResults([
+        [
+          {
+            name: 'foo ⇒ a',
+            iterations: 200,
+            fastest: true,
+            histogram: {
+              "samples": 11,
+              "min": 1966.0067526089626,
+              "max": 2096.9027705175117,
+            },
+            "opsSec": 1989.98778631387,
+          },
+          {
+            name: 'foo ⇒ b',
+            iterations: 200,
+            slowest: true,
+            histogram: {
+              "samples": 12,
+              "min": 1766.0067526089626,
+              "max": 2096.9027705175117,
+            },
+            "opsSec": 1801.98778631387,
+          },
+        ]
+      ]);
+    });
+  });
+
+  describe("toJSON()", () => {
+    it("handles empty results", async () => {
+      let result = Util.toJSON([]);
+
+      expect(result).to.equal("{}");
+    });
+
+    it("generates out results", async () => {
+      let result = Util.toJSON([
+        [
+          {
+            name: 'foo ⇒ a',
+            iterations: 200,
+            fastest: true,
+            histogram: {
+              "samples": 11,
+              "min": 1966.0067526089626,
+              "max": 2096.9027705175117,
+            },
+            "opsSec": 1989.98778631387,
+            "plugins": [
+              "v8-never-optimize=true"
+            ],
+          },
+          {
+            name: 'foo ⇒ b',
+            iterations: 200,
+            slowest: true,
+            histogram: {
+              "samples": 12,
+              "min": 1766.0067526089626,
+              "max": 2096.9027705175117,
+            },
+            "opsSec": 1801.98778631387,
+            "plugins": [
+              "v8-never-optimize=true"
+            ],
+          },
+        ]
+      ]);
+
+      let actual = JSON.parse(result);
+
+      expect(actual).to.have.property('foo');
+    });
+  });
+
+  describe("install()", () => {
+    it("handles packages with slashes", async () => {
+      let result = await Util.install("@babel/core@latest", "@babel/core@latest");
+
+      expect(result.module).to.be.an("object");
+      expect(result.location).to.endWith("/node_modules/@babel/core");
+    });
+  });
+});
